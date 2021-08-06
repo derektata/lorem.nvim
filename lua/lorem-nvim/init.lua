4f4d9de436888d6171b695090b0bad7c654b53cf
Derek Tata <derekjtata@gmail.com>
2021-08-06 17:17:00 -0400

    init

---
+++ lua/lorem-nvim/init.lua
@@ -0,0 +1,19 @@
+local vim = vim
+local cmd = vim.cmd
+local key = vim.api.nvim_set_keymap
+
+local function setup()
+  key('n', 'LI', ':Loremipsum ', {})
+  key('i', 'lorem', '<Esc>:lua require"lorem-nvim".lorem()<CR>', {})
+end
+
+local function lorem()
+  cmd [[
+    Loremipsum
+  ]]
+end
+
+return {
+  setup = setup,
+  lorem = lorem
+}
