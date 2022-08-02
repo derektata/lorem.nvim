710a01d35636acf5f252c5942e70e5963ef43afb
Derek Tata <derekjtata@gmail.com>
2022-08-02 17:05:11 -0400

    Added after/
    Added user command to plugin/init so that users don't need to require a
    setup to use the plugin.

---
+++ after/plugin/init.lua
@@ -0,0 +1,8 @@
+local lorem = require "lorem"
+
+vim.api.nvim_create_user_command("LoremIpsum", function(opts)
+  local number = tonumber(opts.args)
+  vim.api.nvim_paste(lorem.gen_words(number), {}, -1)
+end, { nargs = "*" })
+
+-- require("cmp").register_source("lorem", lorem.new())
