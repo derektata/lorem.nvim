cd7191a8338014612d5fb1d0a04fbcf901ae0fd4
bertmad3400 <57845632+bertmad3400@users.noreply.github.com>
2022-12-28 10:20:10 +0000

    Add support for creating proper sentences

--- lua/lorem/init.lua
+++ lua/lorem/init.lua
@@ -1,8 +1,27 @@
 local source = {}
+local _config = { sentenceLength = "mixedShort" }
+
+local sentenceLengths = {
+	mixed		= {3, 100},
+	mixedLong	= {30, 100},
+	mixedShort	= {3, 30},
+	long		= {40, 60},
+	medium		= {20, 40},
+	short		= {3, 20}
+}
+
+-- Initialize the pseudo random number generator
+math.randomseed( os.time() )
+math.random(); math.random(); math.random()
+
 
 source.new = function()
   local self = setmetatable({}, { __index = source })
   return self
+end
+
+source.setup = function(config)
+	_config = config
 end
 
 source.get_trigger_characters = function()
@@ -13,19 +32,10 @@ source.get_keyword_pattern = function()
   return "^lorem%s+(%d+)$"
 end
 
-source.gen_words = function(number)
+source.gen_sentence = function(length)
   local words = require "lorem.ipsum" ()
-  local length = 100
-
-  if number ~= nil then
-    length = number
-  end
 
   local output = ""
-
-  -- Initialize the pseudo random number generator
-  math.randomseed( os.time() )
-  math.random(); math.random(); math.random()
 
   for i = 1, length do
     output = output .. words[math.random(1, #words)] .. " "
@@ -39,6 +49,36 @@ source.gen_words = function(number)
   output = output:sub(1, -2) .. "."
 
   return output
+end
+
+source.gen_words = function(length)
+
+	if (length == nil) then
+		length = 100
+	end
+
+	local bounds = sentenceLengths[_config["sentenceLength"]]
+
+	local lowerBound = bounds[1]
+	local upperBound = bounds[2]
+
+	local text = ""
+	local textLength = 0
+
+	while ((textLength + upperBound) <= length)
+	do
+		text = text .. source.gen_sentence(math.random(lowerBound, upperBound)) .. " "
+		-- Counting the number of words
+		_, textLength = text:gsub("%S+", "")
+	end
+
+	local remainingSpace = length - textLength
+	if (remainingSpace > 2) then
+		text = text .. source.gen_sentence(remainingSpace)
+	end
+
+	return text
+
 end
 
 -- TODO
